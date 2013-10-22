babc165a9e7f02fc59ce8ce66e18185aabaf124e
osmdawy <osmdawy@gmail.com>
2013-10-22 15:42:24 +0200

    server commented

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -36,6 +36,7 @@ vector<string> split(string str) {
 }
 string readTheFile(string fileName) {
 	string type = "";
+	//check the type if image
 	if (fileName.find(".jpg") != std::string::npos) {
 		type = "image/jpg\r\n";
 	}
@@ -55,6 +56,7 @@ string readTheFile(string fileName) {
 	ifstream myfile(Filename);
 	string fileRead;
 	if (type == "") {
+		//read text and html files
 		if (myfile.is_open()) {
 			fileRead += ("HTTP/1.0 200 OK \r\n");
 			fileRead += "{";
@@ -62,7 +64,7 @@ string readTheFile(string fileName) {
 				//line
 				getline(myfile, line);
 				fileRead += (line);
-				fileRead+="\n";
+				fileRead += "\n";
 			}
 			fileRead += "}";
 			myfile.close();
@@ -91,7 +93,7 @@ string readTheFile(string fileName) {
 			strstream << size;
 			strstream >> number;
 			fileRead += number;
-			fileRead+="\n";
+			fileRead += "\n";
 			fileRead += "{";
 			fileRead += oss.str();
 
@@ -105,34 +107,54 @@ string readTheFile(string fileName) {
 }
 
 int main(int argc, char *argv[]) {
-	int sockfd, newsockfd, portno;
-	socklen_t clilen;
-	char buffer[1024000];
-	struct sockaddr_in serv_addr, cli_addr;
-	int n;
+	int sockfd, newsockfd; //file descriptors one for the socket system call and another for accept system call
+	int portno; //stores the port number on which the server accepts connections.
+	socklen_t clilen; // stores the size of the address of the client
+	char buffer[1024000]; //The server reads characters from the socket connection into this buffer.
+	struct sockaddr_in serv_addr, cli_addr; //a structure containing an internet address
+	int n; //. it contains the number of characters read or written.
 	if (argc < 2) {
 		fprintf(stderr, "ERROR, no port provided\n");
 		exit(1);
 	}
+	//creates a new socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0)
 		error("ERROR opening socket");
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[1]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(portno);
+	bzero((char *) &serv_addr, sizeof(serv_addr)); // sets all values in a buffer to zero
+	portno = atoi(argv[1]); //cast the portno from the arguments
+	serv_addr.sin_family = AF_INET; // a code for the address family
+	serv_addr.sin_addr.s_addr = INADDR_ANY; //This field contains the IP address of the host
+	serv_addr.sin_port = htons(portno); //the port number
+	/**
+	 * The bind() system call binds a socket to an address,
+	 * in this case the address of the current host and port number on which the server will run.
+	 *  It takes three arguments, the socket file descriptor, the address to which is bound, and the size of the address to which it is bound.
+	 */
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR on binding");
 	while (true) {
+		/**The listen system call allows the process to listen on the socket for connections. The first argument is the socket file descriptor,
+		 * and the second is the size of the backlog queue, i.e., the number of connections that can be waiting while the process is handling a particular connection
+		 *
+		 */
 		listen(sockfd, 5);
 		clilen = sizeof(cli_addr);
+		/**
+		 * The accept() system call causes the process to block until a client connects to the server.
+		 * Thus, it wakes up the process when a connection from a client has been successfully established. It returns a new file descriptor
+		 */
 		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
 		if (newsockfd < 0)
 			error("ERROR on accept");
 		pid_t PID = fork();
 		if (PID == 0) {
+			//create the process
 			bzero(buffer, 1024000);
+			/**
+			 *  read() will block until there is something for it to read in the socket,
+			 *   i.e. after the client has executed a write().
+			 */
 			n = read(newsockfd, buffer, 1023999);
 			if (n < 0)
 				error("ERROR reading from socket");
@@ -153,8 +175,11 @@ int main(int argc, char *argv[]) {
 				error("ERROR writing to socket");
 
 			exit(0);
+			//exit the process
 		} else {
+			//the parent closes the socket
 			close(newsockfd);
+
 		}
 
 	}
